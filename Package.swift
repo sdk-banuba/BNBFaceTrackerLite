e7a50be102e7c3bd309ec084c28659e80850dca9
ci <ci@banuba.com>
2023-11-24 17:07:16 +0300

    1.9.1-86-g65c1c833d4

---
+++ Package.swift
@@ -0,0 +1,70 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let bnbPackageVersion: Version = "1.9.1-86-g65c1c833d4"
+
+let package = Package(
+    name: "BNBFaceTrackerLite",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BNBFaceTrackerLite",
+            targets: [
+                "BNBFaceTrackerLite",
+                "BNBFaceTrackerLite_BNBSdkCore",
+                "BNBFaceTrackerLite_BNBEffectPlayer",
+                "BNBFaceTrackerLite_BNBScripting"
+            ]
+        ),
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/sdk-banuba/BNBSdkCore.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBEffectPlayer.git",
+            .exact(bnbPackageVersion)
+        ),
+        .package(
+            url: "https://github.com/sdk-banuba/BNBScripting.git",
+            .exact(bnbPackageVersion)
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "BNBFaceTrackerLite",
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.9.1-86-g65c1c833d4/BNBFaceTrackerLite.zip",
+            checksum: "1df4dd0a80dbf2b6f5f42826fac9fd89e7497c02912b4f3e7cfc962ce3cfe32d"
+        ),
+        .target(
+            name: "BNBFaceTrackerLite_BNBSdkCore",
+            dependencies: [
+                .product(
+                    name: "BNBSdkCore",
+                    package: "BNBSdkCore"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceTrackerLite_BNBEffectPlayer",
+            dependencies: [
+                .product(
+                    name: "BNBEffectPlayer",
+                    package: "BNBEffectPlayer"
+                ),
+            ]
+        ),
+        .target(
+            name: "BNBFaceTrackerLite_BNBScripting",
+            dependencies: [
+                .product(
+                    name: "BNBScripting",
+                    package: "BNBScripting"
+                ),
+            ]
+        ),
+    ]
+)
