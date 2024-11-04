27ac48d4918f3ce318d4e27d6e33a54c20187135
ci <ci@banuba.com>
2024-11-04 20:00:33 +0300

    1.14.1-174-gec051bdc7

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.14.1-173-gec24d1c86"
+let bnbPackageVersion: Version = "1.14.1-174-gec051bdc7"
 
 let package = Package(
     name: "BNBFaceTrackerLite",
@@ -36,8 +36,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BNBFaceTrackerLite",
-            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-173-gec24d1c86/BNBFaceTrackerLite.zip",
-            checksum: "22c76a869382e4a0244c66d7d3a82d77dc02b5102d474ec1b984434016656ca5"
+            url: "https://d2cm7wtcqqw29.cloudfront.net/1.14.1-174-gec051bdc7/BNBFaceTrackerLite.zip",
+            checksum: "2ecfdc15e117c8ec1b516ca2c9328402d5a5c06f2ec03a8c6ca3e7a86136e122"
         ),
         .target(
             name: "BNBFaceTrackerLite_BNBSdkCore",
